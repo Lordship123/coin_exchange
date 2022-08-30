1869399e215663d3ce0669017aa9242a37283b24
Yehor Kharchenko <60078565+Lordship123@users.noreply.github.com>
2022-08-30 17:58:07 +0300

    Upload my first project on React

---
+++ precache-manifest.f28e6b8d1418c18db06933e050dba935.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e1746334c4ac29b0532",
+    "url": "/coin_exchange/static/css/main.1052b93f.chunk.css"
+  },
+  {
+    "revision": "7e1746334c4ac29b0532",
+    "url": "/coin_exchange/static/js/main.7e174633.chunk.js"
+  },
+  {
+    "revision": "2e8f1a6b9f28a667d85d",
+    "url": "/coin_exchange/static/js/1.2e8f1a6b.chunk.js"
+  },
+  {
+    "revision": "f4ac787b452232dc4989",
+    "url": "/coin_exchange/static/js/2.f4ac787b.chunk.js"
+  },
+  {
+    "revision": "d6ca6829cdeddbb38269",
+    "url": "/coin_exchange/static/js/runtime~main.d6ca6829.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/coin_exchange/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "89f62b47578e432138ba2243069e5bbb",
+    "url": "/coin_exchange/index.html"
+  }
+];
